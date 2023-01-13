384f45dbf19f8744a0b81a689fdcf0fddb1365b4
andreivremaroiu <andreivremaroiu225@gmail.com>
2023-01-13 23:31:58 +0200

    stores

---
+++ client/src/stores/index.js
@@ -0,0 +1,34 @@
+import {
+  applyMiddleware,
+  createStore,
+  combineReducers,
+  getState,
+  dispatch,
+} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import userReducer from '../reducers/user';
+import bugReducer from '../reducers/bug';
+import projectReducer from '../reducers/project';
+
+export default function configureStore(preloadedState = {}) {
+  const middlewares = [thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  const composedEnhancers = composeWithDevTools(middlewareEnhancer);
+
+  const reducers = combineReducers({
+    user: userReducer,
+    projects: projectReducer,
+    bugs: bugReducer,
+  });
+  const store = createStore(reducers, preloadedState, composedEnhancers);
+
+  if (process.env.NODE_ENV !== 'production' && module.hot) {
+    module.hot.accept('../reducers/user.js', () =>
+      store.replaceReducer(rootReducer)
+    );
+  }
+
+  return store;
+}
