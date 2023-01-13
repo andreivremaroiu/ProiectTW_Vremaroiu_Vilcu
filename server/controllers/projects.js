4a7dbfe0a717b7598b8d7531b9e1b92caa1c0c8a
andreivremaroiu <andreivremaroiu225@gmail.com>
2023-01-13 21:38:44 +0200

    projects

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -72,21 +72,202 @@ projectsController.updateMembers = async (req, res, next) => {
 };
 
 //eliminare membru proiect
+projectsController.removeMember = (req, res, next) => {
+    const {projectId, userId} = req.body;
+
+    const sqlQuery = `
+        delete from memberlist
+        where user_id = ${userId} and project_id = ${projectId}
+        returning userId
+    `;
+
+    db.query(sqlQuery).then(results => {
+        res.locals.data = results.rows;
+        next();
+    }).catch(err => next(err, req, res))
+}
 
 //numar buguri proiect
+projectsController.getBugsCount = async (req, res, next) => {
+    const {projectId} = req.params;
+
+    const sqlQuery = `
+        select count (*) from bugs where project_id = ${projectId}
+    `
+
+    db.query(sqlQuery).then(results => {
+        res.locals.data = results.rows;
+        next();
+    }).catch(err => next(err, req, res));
+}
 
 //selecteaza toate bugurile proiect
+projectsController.getBugs = async (req, res, next) => {
+    const {projectId} = req.params;
+    const {limit, offset} = req.query;
+
+    const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
+    const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
+
+    const sqlQuery = `
+        select * from bugs where project_id = ${projectId} order by bugs.id
+        ${limitClause}
+        ${offsetClause}
+    `;
+
+    db.query(sqlQuery)
+    .then(results => {
+        res.locals.data = results.rows;
+        next();
+    }).catch(err => next(err, req, res));
+}
 
 //nr total proiecte
+projectsController.getAllProjectsCount = (req, res, next) => {
+    const sqlQuery = `
+        select count(*) from projects
+    `;
+
+    db.query(sqlQuery).then(results => {
+        res.locals.data = results.rows;
+        next();
+    }).catch(err => next(err, req, res));
+};
+
+//selecteaza toate proiectele
+projectsController.getAllProjects = (req, res, next) => {
+    const {limit, offset} = req.query;
+
+    const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
+    const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
+
+    const sqlQuery = `select * from projects order by projects.id ${limitClause} ${offsetClause}`;
+
+    db.query(sqlQuery).then(results => {
+        res.locals.dat = results.rows;
+        next();
+    }).catch(err => next(err, req, res));
+};
 
 //creare proiect nou
+projectsController.create = async (req, res, next) => {
+    const {name, owner, users} = req.body;
+
+    //creare proiect si selectare id proiect
+    const sqlQuery = `insert into project (name, owner) values ('${name}', ${owner}) returning id, name, owner`
+
+    let projectId;
+
+    await db.query(sqlQuery).then(results => {
+        projectId = results.rows[0].id;
+        res.locals.data = results.rows;
+    }).catch(err => next(err, req, res));
+
+    //creare lista membri
+    if(users.length > 0) {
+        const values = users.map(userId => `(${userId}, ${projectId})`).join(',');
+
+        const sqlQuery = `insert into memberlist (user_id, project_id)
+        values ${values} returning user_id`;
+
+        await db.query(sqlQuery).then(results => {
+            res.locals.members = results.rows;
+        }).catch(err => next(err, req, res));
+    }
+    next();
+};
 
 //afisare detalii proiect
+projectsController.get = (req, res, next) => {
+    const {projectId} = req.params;
+  
+    const sql = `
+      SELECT *
+      FROM projects
+      WHERE id = ${projectId}`;
+  
+    db.query(sql)
+      .then(results => {
+        res.locals.data = results.rows;
+        next();
+      })
+      .catch(error => next(error, req, res));
+};
 
 //update detalii proiect
+projectsController.update = (req, res, next) => {
+    const {projectId, name, owner} = req.body;
+  
+    const sql = `
+      UPDATE projects
+      SET name = '${name}', owner = ${owner}
+      WHERE id = ${projectId}
+      RETURNING id, name, owner`
+  
+    db.query(sql)
+      .then(results => {
+        res.locals.data = results.rows;
+        next();
+      })
+      .catch(error => next(error, req, res));
+};
 
 //stergere proiect 1
+projectsController.delete = async (req, res, next) => {
+    const {projectId} = req.body;
+
+    //stergere lista membri
+    let sql = `
+      DELETE FROM memberlist
+      WHERE project_id = ${projectId}`;
+    
+    await db.query(sql)
+      .then(results => {
+        res.locals.data = results.rows;
+      })
+      .catch(error => next(error, req, res)); 
+  
+    // stergere proiect
+    sql = `
+      DELETE FROM projects
+      WHERE id = ${projectId}
+      RETURNING id`;
+  
+    db.query(sql)
+      .then(results => {
+        res.locals.data = results.rows;
+        next();
+      })
+      .catch(error => next(error, req, res));    
+  };
 
 //stergere proiect 2
+projectsController.deleteProject = async (req, res, next) => {
+    const {projectId} = req.params;
 
+    //stergere lista membri
+    let sql = `
+      DELETE FROM memberlist
+      WHERE project_id = ${projectId}`;
+    
+    await db.query(sql)
+      .then(results => {
+        res.locals.data = results.rows;
+      })
+      .catch(error => next(error, req, res)); 
+  
+    // stergere proiect
+    sql = `
+      DELETE FROM projects
+      WHERE id = ${projectId}
+      RETURNING id`;
+  
+    db.query(sql)
+      .then(results => {
+        res.locals.data = results.rows;
+        next();
+      })
+      .catch(error => next(error, req, res));    
+  };
 
+  module.exports = projectsController;
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -3,3 +3,93 @@ const projectsController = require('../controllers/projects');
 
 const router = express.Router();
 
+//selecteaza toti membrii proiect
+router.get('/getMembers/:projectId', projectsController.getMembers, (req, res) => {
+    res.status(200).json(res.locals.data);
+})
+
+//adaugare membru
+router.post('/addMember', projectsController.addMember, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//update membri
+router.post('/updateMembers', projectsController.updateMembers, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//stergere membru
+router.delete('/removeMember', projectsController.removeMember, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//Aflare nr total buguri proiect
+router.get('/getBugsCount/:projectId', projectsController.getBugsCount, (req, res) => {
+    res.status(200).json(res.locals.data);
+})
+
+//Selectare toate bugurile dintr-un proiect
+router.get('/getBugs/:projectId', projectsController.getBugs, (req, res) => {
+    res.status(200).json(res.locals.data);
+})
+
+//nr total proiecte
+router.get('/getAllProjectsCount', projectsController.getAllProjectsCount, (req, res) => {
+    res.status(200).json(res.locals.data);
+})
+
+//selectare proiecte
+router.get('/getAllProjects', projectsController.getAllProjects, (req, res) => {
+    res.status(200).json(res.locals.data);
+})
+
+//selectare nr total proiecte pe calea /count
+router.get('/count', projectsController.getAllProjectsCount, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//nr buguri proiect
+router.get('/:projectId/bugs/count', projectsController.getBugsCount, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//selectare buguri proiect
+router.get('/:projectId/bugs', projectsController.getBugs, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//selectare membrii proiect
+router.get('/:projectId/members', projectsController.getMembers, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//selectare detalii proiect
+router.get('/:projectId', projectsController.get, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//selectare toate proiectele
+router.get('/', projectsController.getAllProjects, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//creare proiect nou
+router.post('/create', projectsController.create, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//update detalii proiect
+router.post('/update', projectsController.update, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+//stergere proiect
+router.delete('/delete', projectsController.delete, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+router.delete('/:projectId', projectsController.deleteProject, (req, res) => {
+    res.status(200).json(res.locals.data);
+});
+
+module.exports = router;
